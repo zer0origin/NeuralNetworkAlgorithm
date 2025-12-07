48e5fc4b38f11f7f15e5526ebd54af25be11133f
Callum <callumery11@outlook.com>
2025-12-07 02:21:40 +0000

    setup sigmoid, matrix dot product, and testing

---
+++ neuralnetwork.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"errors"
+	"math"
+)
+
+type Matrix [][]float64
+
+var IncompatibleMatrix = errors.New("incompatible Matrix, operation cannot be performed")
+var EmptyMatrix = errors.New("matrix is empty")
+
+func NewMatrix(rows uint32, columns uint32) Matrix {
+	return func() [][]float64 {
+		zeroArr := make([][]float64, rows)
+		for r := range rows {
+			for range columns {
+				zeroArr[r] = append(zeroArr[r], 0)
+			}
+		}
+
+		return zeroArr
+	}()
+}
+
+func NewMatrixFromValues(values ...[]float64) Matrix {
+	return values
+}
+
+// GetMatrixRow row is 0 indexed.
+func (a Matrix) GetMatrixRow(row uint32) []float64 {
+	return a[row]
+}
+
+func (a Matrix) GetMatrixColumn(column uint32) []float64 {
+	rowLen := len(a)
+	resultArr := make([]float64, rowLen)
+
+	for x := range rowLen {
+		resultArr[x] = a[x][column]
+	}
+
+	return resultArr
+}
+
+func (a Matrix) GetMatrixColumnAsMatrix(column uint32) Matrix {
+	rowLen := len(a)
+	resultArr := make([][]float64, rowLen)
+
+	for x := range rowLen {
+		resultArr[x] = append(resultArr[x], a[x][column])
+	}
+
+	return resultArr
+}
+
+func (a Matrix) DotProduct(b Matrix) (Matrix, error) {
+	rowsInA := len(a)
+	columnInA := len(a[0])
+	columnInB := len(b[0])
+	rowsInB := len(b)
+
+	if rowsInA == 0 || columnInB == 0 {
+		return Matrix{}, EmptyMatrix
+	}
+
+	if columnInA != rowsInB {
+		return Matrix{}, IncompatibleMatrix
+	}
+
+	result := NewMatrix(uint32(rowsInA), uint32(columnInB))
+
+	for r := range rowsInA {
+		for c := range columnInB {
+			row := a.GetMatrixRow(uint32(r))
+			column := b.GetMatrixColumn(uint32(c))
+
+			for n := range columnInA {
+				result[r][c] = result[r][c] + row[n]*column[n]
+			}
+		}
+	}
+
+	return result, nil
+}
+
+func (a Matrix) ApplyFunc(toApply func(float64) float64) {
+	rowsOfA := len(a)
+	colOfA := len(a[0])
+
+	for r := range rowsOfA {
+		for c := range colOfA {
+			a[r][c] = toApply(a[r][c])
+		}
+	}
+}
+
+func Sigmoid(x float64) float64 {
+	return 1 / (1 + math.Pow(math.E, -x))
+}
+
+//https://www.jeremyjordan.me/intro-to-neural-networks/
---
+++ neuralnetwork_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMatrixProduct(t *testing.T) {
+	matrixA := NewMatrix(2, 3)
+	rowZero := matrixA.GetMatrixRow(0)
+	rowZero[0] = 1
+	rowZero[1] = 2
+	rowZero[2] = 3
+
+	rowOne := matrixA.GetMatrixRow(1)
+	rowOne[0] = 4
+	rowOne[1] = 5
+	rowOne[2] = 6
+
+	matrixB := NewMatrix(3, 2)
+	browZero := matrixB.GetMatrixRow(0)
+	browZero[0] = 7
+	browZero[1] = 8
+
+	browOne := matrixB.GetMatrixRow(1)
+	browOne[0] = 9
+	browOne[1] = 10
+
+	browTwo := matrixB.GetMatrixRow(2)
+	browTwo[0] = 11
+	browTwo[1] = 12
+
+	product, err := matrixA.DotProduct(matrixB)
+	require.NoError(t, err)
+
+	expected := NewMatrixFromValues([]float64{58, 64}, []float64{139, 154})
+	assert.Equal(t, expected, product)
+}
+
+func TestThreeLayerNeural(t *testing.T) {
+	//weight * input + bias, -->> activation function
+	i := NewMatrixFromValues([]float64{0.9}, []float64{0.1}, []float64{0.8})
+	inputHidden := NewMatrixFromValues([]float64{0.9, 0.3, 0.4}, []float64{0.2, 0.8, 0.2}, []float64{0.1, 0.5, 0.6})
+	hiddenOutput := NewMatrixFromValues([]float64{0.3, 0.7, 0.5}, []float64{0.6, 0.5, 0.2}, []float64{0.8, 0.1, 0.9})
+
+	product, err := inputHidden.DotProduct(i)
+	require.NoError(t, err)
+	fmt.Printf("Before Sigmoid (product): %.4v\n", product)
+	product.ApplyFunc(Sigmoid)
+	fmt.Printf("After Sigmoid (product): %.4v\n", product)
+
+	dotProduct, err := hiddenOutput.DotProduct(product)
+	require.NoError(t, err)
+	fmt.Printf("Before Sigmoid (dotProduct): %.4v\n", dotProduct)
+	dotProduct.ApplyFunc(Sigmoid)
+	fmt.Printf("After Sigmoid (dotProduct): %.4v\n", dotProduct)
+	fmt.Printf("%.3v\n", dotProduct)
+}
